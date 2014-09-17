31f83828971005c6ad7cec651ce1e47ad73173e0
JJohnson1988 <mythixus@gmail.com>
2014-09-17 04:26:55 -0700

    Revert "Re-initial commit"
    This reverts commit 277bef9fb563750462cb37687ae8c5ca31cbd50a.

---
+++ app/build/generated/source/buildConfig/debug/me/jjohnson1988/android/xposed/removeactionoverflowbutton/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package me.jjohnson1988.android.xposed.removeactionoverflowbutton;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "me.jjohnson1988.android.xposed.removeactionoverflowbutton";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 6;
+  public static final String VERSION_NAME = "1.5";
+}
